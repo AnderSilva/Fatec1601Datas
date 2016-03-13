36f1e93a8c8fc4b057c317514f32b2c5f9545827
AnderSilva <anderson.goma@gmail.com>
2016-03-13 19:27:16 -0300

    Inicio da classe java

---
+++ src/Datas.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author ander
+ *
+ */
+public class Datas {
+	//Variaveis de instancia
+	
+	//Construtores
+	public Data(){}
+	public Data(int dia, int mes, int ano ){}
+	
+	
+	//Metodos
+	
+	
+	
+	
+
+}
