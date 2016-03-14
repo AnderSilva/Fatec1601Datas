f505ddc87d4c689f0e65fae3c28c51d53fc0b155
AnderSilva <anderson.goma@gmail.com>
2016-03-13 23:18:39 -0300

    Adicionado Classe de teste

---
+++ src/Teste.java
@@ -0,0 +1,14 @@
+
+public class Teste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Data data = new Data(14,3,2016); 
+		System.out.print(data.stringData("14/03/2016"));
+	}
+
+}
