d115682ec231f3a30dd069a935a08c3b13357acc
AnderSilva <anderson.goma@gmail.com>
2016-03-21 00:27:19 -0300

    Correcao de erros de compilacao diversos
    adaptacao das novas rotinas inseridas
    
    Variaveis para obtencao de separador , indexando pela variavel formato
    
    Troca de dia, mes, ano para componentes[0],[1] e [2] respectivamente
    
    Implementacao de mudaFormato (testar)
    
    Criacao dos metodos getStringData e ConsisteData

--- src/Data.java
+++ src/Data.java
@@ -4,37 +4,73 @@
  *
  */
 public class Data {
-	//Variaveis de Classe
-	static int formato = 0;
-	static String separador = "/";
-	int ano = 0;
-	int mes = 0;
-	int dia = 0;
+	//Variaveis de Classe	
+	static int  formato   = 0;
+	// 0 - Britânico: DD/MM/AAAA
+	// 1 - Americano: MM/DD/AAAA
+	// 2 - Italiano : DD-MM-AAAA
+	// 3 - Germânico: DD.MM.AAAA
+	// 4 - ANSI     : AAAA.MM.DD
+	static char separador = '/';	
+	static int termos[] = {0, 1, 2};
+	
+	//indice dos vetores abaixo correspondem ao separador e formato (entre 0 e 4) respectivo
+	private static char vetsep[] = { '/' , '/' , '-' , '.' , '.' }; 
+	private static char vetfor[][]  = { {0,1,2}, {1,0,2}, {0,1,2}, {0,1,2}, {2,1,0} };
+	
 	
 	//Variaveis de instancia
-	static int componentes[]
-	static int termos[] = {0, 1, 2}
+	int  componentes[] = {0, 0, 0}; // dia, mes, ano	
+	
+	//Exemplo de montagem da string da data:
+	//“” + componentes[termos[0]]+separador+componentes[termos[1]]+separador+componentes[termos[2]];
 		
-	//Construtores
-	public Data(){}
-	public Data(int dia, int mes, int ano ){
-		this.ano = ano;
-		this.mes = mes;
-		this.dia = dia;
+	//Construtores	
+	public Data(int dia, int mes, int ano){
+		this.componentes[0] = dia;
+		this.componentes[1] = mes;
+		this.componentes[2] = ano;		
 	}
-	public Data(Data d) {
-		this.ano = d.ano;
-		this.mes = d.mes;
-		this.dia = d.dia;
+	public Data(String data){		
+		//Recebe uma string e 
+		//atribui cada termo no vetor de componentes
+		//conforme o formato que estiver atualmente
+		
+		//1.o splitar (separar a string) usando o separador e descobrir quem é dia,mes,ano		
+		//2.o preencher o vetor de componentes
 	}
 	
+	public Data(Data d) {
+		componentes[0] = d.componentes[0];
+		componentes[1] = d.componentes[1];
+		componentes[2] = d.componentes[2];		
+	}
+	
+	
+	private String getStringData(){ // testar
+		//Recebe a data instanciada e retorna uma string de data
+		//Exemplo 13,6,2016 retornaria 13/06/2016 no formato britanico (0)
+		
+		return  "" 	+ this.componentes[ vetfor[ formato ][0] ] + separador
+					+ this.componentes[ vetfor[ formato ][1] ] + separador
+					+ this.componentes[ vetfor[ formato ][2] ];
+	}
+
 	
 	//Metodos 
-	static boolean bissexto(int ano){
-		if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
-			return true;
-		}
-	return false;
+	public boolean ConsisteData(Data d){ //testar
+		// Esse método recebe uma variavel do tipo Data e apos 
+		// passar por validações, retorna True se for uma data válida
+		String strdata = getStringData();
+		
+		if (!this.stringData( strdata )) return false; //Se stringData for falso, retorna falso
+										
+		int diasM = diasMes(this.componentes[0],this.componentes[2]);
+		return (diasM>=28 && diasM<=31); //se diasMes estiver fora do range => falso		
+	}
+		
+	static boolean bissexto(int ano){ //ok
+		return ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0));	
 	}	
 
 	static int diasMes(int mes, int ano){
@@ -52,42 +88,56 @@ public class Data {
 		return 0;	
 	}
 	
-	static boolean mudaFormato(int f){
-		// retorna false se o formato for invalido
-		return false;
+	static boolean mudaFormato(int f){ //testar
+		
+		if ( (f<0) || (f>4) ) return false;
+		
+		formato = f;
+		separador = vetsep[formato];
+		
+		return true;
 	}
 	
-	public boolean stringData(String s){
-		int k=0;
-		for (int i=0 ; i==strdata.lenght() ; i++)
-		{
-   			if ((strdata.charAt(i) == separador) && (k==0))
-   			{
-   				sub1 = strdata.substring(0, i)
-				pos1 = i
-				k++
-   			}
-   			else if ((k==1) && (strdata.charAt(i) == separador))
-			{
-   				sub2 = strdata.substring(pos1+1, i)
-			        pos2 = i
-			   	k++
-		         }
-   			else	if (k == 2)
-   				sub3 = strdata.substring(pos2+1)
-			else 
-   				return false;	        
-		} 
-		componentes[termos[0]] = Integer.parseInt(sub1);
-		componentes[termos[1]] = Integer.parseInt(sub2);
-		componentes[termos[2]] = Integer.parseInt(sub3);
-		return true;
+	public boolean stringData(String s){ //testar
 		/*
 		 * A instancia que ativa este método deve assumir o valor 
 		 * correspondente à string recebida como parâmetro. 
 		 * A data passará pela consistencia, 
 		 * retornará true para data OK, e false para data inválida.
 		 */
+		
+		String strdata = getStringData(); 
+		
+		int k=0, 
+			pos1 = 0,
+			pos2 = 0;
+		String sub1 = null, 
+			   sub2 = null,sub3 = null;
+		
+		for (int i=0 ; i==strdata.length(); i++)
+		{
+   			if ((strdata.charAt(i) == separador) && (k==0)) {
+   				sub1 = strdata.substring(0, i);
+				pos1 = i;
+				k++;
+   			}
+   			else if ((k==1) && (strdata.charAt(i) == separador)) {
+   				sub2 = strdata.substring(pos1+1, i);
+		        pos2 = i;
+			   	k++;
+		    }
+   			else if (k == 2){
+   				sub3 = strdata.substring(pos2+1);
+   			}
+			else 
+   				return false;	        
+		} 
+		
+		componentes[ vetfor[ formato ][0] ] = Integer.parseInt(sub1);
+		componentes[ vetfor[ formato ][1] ] = Integer.parseInt(sub2);
+		componentes[ vetfor[ formato ][2] ] = Integer.parseInt(sub3);
+		return true;
+		
 	}
 	
 	public String dataString(){
@@ -99,12 +149,12 @@ public class Data {
 	}	
 	public Data soma(int dias){
 		// Soma: Data + dias => Outra Data posterior em dias
-		Data dt = new Data();
+		Data dt = new Data(12,3,2016);
 		return dt;
 	}
 	public Data sub(int dias){
 		//Subtração 1: Data – dias => Data anterior em dias
-		Data dt = new Data();
+		Data dt = new Data(13,3,2016);
 		return dt;
 	}
 	public long sub(Data d){
@@ -114,10 +164,11 @@ public class Data {
 	}
 	
 	//Metodos auxiliares privativos	
+	@SuppressWarnings("unused")
 	private long dataDias() {
 		int bis = 0;											
 		int noBis = 0;
-		for(int anoInicial = 1900; anoInicial < this.ano; anoInicial++) {
+		for(int anoInicial = 1900; anoInicial < this.componentes[2]; anoInicial++) {
 			if ( bissexto(anoInicial)) {						
 				bis+=1;											
 				anoInicial++;
@@ -128,17 +179,21 @@ public class Data {
 			}
 		}
 		int diasAnos = (bis * 366) + (noBis * 365);
-	
+		
+		int diasMeses = 0;
+		
+		int mes = this.componentes[1];
 		while (mes != 0) {
-			int diasMeses = 0;
-			diasMeses += diasMes(mes);   //???;  	(variável diasMeses que conterá o valor do total de dias dos meses)
+			
+			diasMeses += diasMes(mes, this.componentes[2]);   //???;  	(variável diasMeses que conterá o valor do total de dias dos meses)
 			mes--;		//	(problema: colocar outra variável com os dias de cada mes pra somar)
 		}
 
-		//long totalDias = diasAnos + diasMeses + dia;
-		return 100;
+		long totalDias = diasAnos + diasMeses + this.componentes[0];
+		return totalDias;
 	}
 	
+	@SuppressWarnings("unused")
 	private void diasData(long d){
 		/*
 		 * A partir de d, que representa dias desde 1-jan-1900, 
--- src/Teste.java
+++ src/Teste.java
@@ -7,17 +7,21 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 		Data data = new Data(14,3,2016);
 		// bissexto ok
-		System.out.println(Data.bissexto(data.ano));
+		System.out.println(Data.bissexto(data.componentes[2]));
+		
 		// Instancia da data ok
-		System.out.println(""+data.ano+"-"+data.mes+"-"+data.dia);
+		System.out.println(""+data.componentes[0]+"-"+data.componentes[1]+"-"+data.componentes[2]);
+		
 		// Teste de DiasMes
 		System.out.println(Data.diasMes(13, 2016));
-		System.out.println(Data.diasMes(data.mes, data.ano));
+		System.out.println(Data.diasMes(data.componentes[1], data.componentes[2]));
+		
 		
 		Data dtbx = new Data(14,2,2016);
-		System.out.println(Data.diasMes(dtbx.mes, dtbx.ano));
+		System.out.println(Data.diasMes(dtbx.componentes[1], dtbx.componentes[2]));
 	}
 
 }
