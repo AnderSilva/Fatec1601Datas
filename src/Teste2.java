bd00004fe927d9bb5558cd81c70e355efdd8b8cf
Carolina de Lima Lopes <carolinalopes@outlook.com>
2016-03-23 21:23:16 -0300

    Update Teste2.java

--- src/Teste2.java
+++ src/Teste2.java
@@ -1,7 +1,55 @@
 import java.util.Date;
 
-public class Teste2
-{
+public class Teste2 {
+	
+	private void diasData(long d){
+		if ( d >= 366 ) {											
+		// verifica se d e maior que o numero de um ano bissexto
+			int numeroAnos = d / 366;								
+			// conta mais ou menos quantos anos inteiros tem
+			// tem que pegar a parte inteira do decimal
+			int anoAnterior = 1900 + numeroAnos;					
+			// verifica o intervalo de anos, de 1900 ate x anos
+			for ( int i = 1900 ; i <= anoAnterior ; i++ ) {			
+			// para 1900 ate x anos, vai incrementando
+				if ( bissexto(i) ) {				 				
+				// se e bissexto
+					int d-= 366;									
+					// e subtraido de d o valor de 366 dias
+				}
+				else {
+					int d-= 365;									
+					// senao e subtraido de d o valor de 365 dias
+				}
+			} return d;												
+			// retorna o resto da subtracao
+		}
+		else {
+			while ( d != 0 ) {										
+			// enquanto d for diferente de zero
+				int ano = anoAnterior + 1;  				
+				// ano atual e o anterior mais um
+				int i = 1;  							
+				// i recebe um que sera referente ao mes de janeiro
+				bissexto(ano);										
+				// chama bissexto
+				diasMes(i);								
+				// chama diasMes para i
+				i++;
+				// incrementa i
+				// TODO: como saber quando parar se verificar os meses, quando o d for menor de 30 ou 28
+				// TODO: verificar o resto dos dias.
+				}
+		}
+		int dia = ?;
+		// resto do dia que e o dia da data atual
+		int mes = i;
+		// mes que parou a verificacao
+		int ano = ano;
+		// ano atual da data
+	}
+	
+	
 	public Data soma(int dias){
 		//  TODO: Soma: Data + dias => Outra Data posterior em dias
 		int ano = 2004;
@@ -59,6 +107,8 @@ public class Teste2
 		//Data dt = new Data();
 		//return dt;
 	}
+	
+	
 	public static void main(String[] args){
 		Date data = new Date();
 		System.out.println(data);
