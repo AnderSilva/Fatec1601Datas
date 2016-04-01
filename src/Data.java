59a18c29d71880d5c0d74f61adfde9629964090f
AnderSilva <anderson.gomes@praticare.com.br>
2016-03-31 23:10:49 -0300

    versao final v0 Data.java

--- src/Data.java
+++ src/Data.java
@@ -1,16 +1,23 @@
 
 /**
- * @author 
- *
+ * @authores
+ * 		ANGELICA ROCHA - 14201566
+ * 	    ANDERSON SILVA - 15213661
+ * 		CAROLINA LOPES - 14211778
+ * 		DEVIDE         - 
+ * 		JESSICA VILELA - 14201575
+ * 		SILVIO LUIS JR - 14211811
+ * 
  */
 public class Data {
-	//Variaveis de Classe	
-	static int  formato   = 0;
 	// 0 - Britânico: DD/MM/AAAA
 	// 1 - Americano: MM/DD/AAAA
 	// 2 - Italiano : DD-MM-AAAA
 	// 3 - Germânico: DD.MM.AAAA
 	// 4 - ANSI     : AAAA.MM.DD
+	
+	//Variaveis de Classe	
+	static int  formato   = 0;
 	static char separador = '/';	
 	static int termos[] = {0, 1, 2};
 	
@@ -18,13 +25,9 @@ public class Data {
 	private static char vetsep[] = { '/' , '/' , '-' , '.' , '.' }; 
 	private static char vetfor[][]  = { {0,1,2}, {1,0,2}, {0,1,2}, {0,1,2}, {2,1,0} };
 	
-	
 	//Variaveis de instancia
-	int  componentes[] = {0, 0, 0}; // dia, mes, ano	
-	
-	//Exemplo de montagem da string da data:
-	//“” + componentes[termos[0]]+separador+componentes[termos[1]]+separador+componentes[termos[2]];
-		
+	int  componentes[] = {0, 0, 0}; // dia, mes, ano
+
 	//Construtores
 	public Data(int dia, int mes, int ano){
 		this.componentes[0] = dia;
@@ -34,12 +37,7 @@ public class Data {
 		if (!ConsisteData() ){
 			zeraComponentes();
 		}
-	}
-	
-	private void zeraComponentes(){
-		for (int i=0;i<=2;i++){
-			this.componentes[i] = 0;	
-		}
+		
 	}
 	
 	public Data(String data){
@@ -47,63 +45,16 @@ public class Data {
 		if (!stringData(data)){
 			zeraComponentes();
 		}
+		
 	}
-	
 	public Data(Data d) {
 		componentes[0] = d.componentes[0];
 		componentes[1] = d.componentes[1];
 		componentes[2] = d.componentes[2];		
 	}
 	
-	
-	public String getStringData(){ // testar
-		//Recebe a data instanciada e retorna uma string de data
-		//Exemplo 13,6,2016 retornaria 13/06/2016 no formato britanico (0)
-		
-		return  "" 	+ this.componentes[ vetfor[ formato ][0] ] + separador
-					+ this.componentes[ vetfor[ formato ][1] ] + separador
-					+ this.componentes[ vetfor[ formato ][2] ];
-	}
-
-	
-	//Metodos 
-	public boolean ConsisteData(){ //testar
-		// Esse método efetua validações, retorna True se for uma data válida
-										
-		int diasM = diasMes(this.componentes[1],this.componentes[2]);
-		return (diasM!=0); //se diasMes estiver fora do range => falso		
-	}
-		
-	static boolean bissexto(int ano){ //ok
-		return ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0));	
-	}	
-
-	static int diasMes(int mes, int ano){
-		//Retorna quantos dias tem o mes m informado e 0 quando houver erro
-		
-		if ((mes == 2) && (bissexto(ano))) {
-			return 29;
-		}
-
-		if ((mes >=1) && (mes <=12)) {
-			int meses [] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-			return meses[mes-1];
-		}
-
-		return 0;
-	}
-	
-	static boolean mudaFormato(int f){ //testar
-		
-		if ( (f<0) || (f>4) ) return false;
-		
-		formato = f;
-		separador = vetsep[formato];
-		
-		return true;
-	}
-	
-	public boolean stringData(String strdata){ //testar
+	//Metodos
+	public boolean stringData(String s){
 		/*
 		 * A instancia que ativa este método deve assumir o valor 
 		 * correspondente à string recebida como parâmetro. 
@@ -117,29 +68,30 @@ public class Data {
 		String sub1 = null, 
 			   sub2 = null,sub3 = null;
 		
-		for (int i=0 ; i==strdata.length(); i++)
-		{
-   			if ((strdata.charAt(i) == separador) && (k==0)) {
-   				sub1 = strdata.substring(0, i);
+		for (int i=0 ; i==s.length(); i++) {
+   			if ((s.charAt(i) == separador) && (k==0)) {
+   				sub1 = s.substring(0, i);
 				pos1 = i;
 				k++;
    			}
-   			else if ((k==1) && (strdata.charAt(i) == separador)) {
-   				sub2 = strdata.substring(pos1+1, i);
+   			else if ((k==1) && (s.charAt(i) == separador)) {
+   				sub2 = s.substring(pos1+1, i);
 		        pos2 = i;
 			   	k++;
 		    }
    			else if (k == 2){
-   				sub3 = strdata.substring(pos2+1);
+   				sub3 = s.substring(pos2+1);
    			}
 			else 
-   				return false;	        
+   				return false;
+   			
 		} 
 		// Chegando aqui, os separadores estao ok
 		// chamaremos o restante da consistencia
 		this.componentes[0] = Integer.parseInt(sub1);
 		this.componentes[1] = Integer.parseInt(sub2);
 		this.componentes[2] = Integer.parseInt(sub3);
+		
 		return ConsisteData();		
 	}
 	
@@ -148,60 +100,197 @@ public class Data {
 		 * Retorna uma string a partir do conteúdo da instancia que 
 		 * ativa o método, de modo que seja respeitado o formato
 		 */
-		return "15/02/2016";
+		return  "" 	+ this.componentes[ vetfor[ formato ][0] ] + vetsep[formato]
+				    + this.componentes[ vetfor[ formato ][1] ] + vetsep[formato]
+				    + this.componentes[ vetfor[ formato ][2] ];
 	}	
-	public Data soma(int dias){
-		// Soma: Data + dias => Outra Data posterior em dias
-		Data dt = new Data(12,3,2016);
-		return dt;
-	}
-	public Data sub(int dias){
-		//Subtração 1: Data – dias => Data anterior em dias
-		Data dt = new Data(13,3,2016);
-		return dt;
-	}
-	public long sub(Data d){
-		//Subtração 2: Data – Data => dias decorridos entre as duas datas.
-		long sub=1000;
-		return sub;
-	}
 	
-	//Metodos auxiliares privativos	
 	@SuppressWarnings("unused")
 	private long dataDias() {
-		int bis = 0;											
+		/* Fornece um valor long que significa o nº de dias decorridos desde 
+		 * 1 de janeiro de 1900 até a data conteúdo da instancia
+		 */
+		int bis = 0;
 		int noBis = 0;
-		for(int anoInicial = 1900; anoInicial < this.componentes[2]; anoInicial++) {
-			if ( bissexto(anoInicial)) {						
-				bis+=1;											
-				anoInicial++;
-			}
-			else {												
-				noBis+=1;										
-				anoInicial++;
-			}
+		
+		for(int ano = 1900; ano < this.componentes[2]; ano++) {			
+			if ( bissexto(ano) ) bis+=1;			
+			else noBis+=1;
 		}
 		int diasAnos = (bis * 366) + (noBis * 365);
 		
 		int diasMeses = 0;
-		
-		int mes = this.componentes[1];
-		while (mes != 0) {
-			
-			diasMeses += diasMes(mes, this.componentes[2]);   //???;  	(variável diasMeses que conterá o valor do total de dias dos meses)
-			mes--;		//	(problema: colocar outra variável com os dias de cada mes pra somar)
+		for (int mes=1; mes< this.componentes[1]; mes++){					
+			diasMeses += diasMes(mes);
 		}
 
 		long totalDias = diasAnos + diasMeses + this.componentes[0];
 		return totalDias;
 	}
-	
+		
 	@SuppressWarnings("unused")
 	private void diasData(long d){
-		/*
-		 * A partir de d, que representa dias desde 1-jan-1900, 
-		 * define o conteúdo da instancia que ativa o método
-		 */
+	/*
+	 * A partir de d, que representa dias desde 1-jan-1900, 
+	 * define o conteúdo da instancia que ativa o método
+	 */
+		int dia = 1;
+		int mes = 1;
+		int ano = 1900;
+		
+		for (long dias = d; dias > 1; dias--) {
+	        int diasNoMes = diasMes(mes,ano);
+	        
+	        if (dia < diasNoMes) dia++;
+	        else if (mes < 12) {
+	            dia = 1;
+	            mes++;
+	        } else {
+	            dia = 1;
+	            mes = 1;
+	            ano++;
+	        }
+	    }
+		
+	}
+	
+	private void zeraComponentes(){
+		
+		for (int i=0;i<=2;i++) {
+			this.componentes[i] = 0;	
+		}
+		
+	}
+
+	//Metodos 
+	public boolean ConsisteData(){
+		// Esse método efetua validações, retorna True se for uma data válida
+		int dia = this.componentes[0];
+		int mes = this.componentes[1];
+		int ano = this.componentes[2];
+		
+		int diasM = diasMes(mes,ano);
+				
+		return ( diasM!=0 && (dia >0 && dia <= diasM) );
+	}
+		
+	static boolean bissexto(int ano){ 
+		
+		return ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0));
+		
+	}	
+
+	static int diasMes(int mes){
+		//Retorna quantos dias tem o mes m informado e 0 quando houver erro
+		
+		if ((mes >=1) && (mes <=12)) {
+			int meses [] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+			return meses[mes-1];
+		}
+
+		return 0;
+	}
+	
+	static int diasMes(int mes, int ano){
+		/* Retorna quantos dias tem o mes informado, considerando o ano para
+		 * checagem de bissexto.
+		 * Qualquer erro é retornado zero. 
+		 */	
+		int meses [] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		if ( (mes == 2) && ( bissexto(ano) ) ) return 29;		
+		if ( (mes >= 1) && ( mes <=12) )       return meses[mes-1];
+		
+		return 0;
+	}
+	
+	static boolean mudaFormato(int f){
+		
+		if ( (f<0) || (f>4) ) return false;
+		
+		formato = f;
+		separador = vetsep[formato];
+		
+		return true;
+	}
+		
+	public Data soma(int dias){
+		// Soma: Data + dias => Outra Data posterior em dias
+		int dia = this.componentes[0];
+		int mes = this.componentes[1];
+		int ano = this.componentes[2];
+		
+        for (int idia = dias; idia > 1; idia--) {
+            int diasNoMes = diasMes(mes,ano);
+            
+            if (dia < diasNoMes) dia++;
+            else if (mes < 12) {
+                dia = 1;
+                mes++;
+            } 
+            else {
+                dia = 1;
+                mes = 1;
+                ano++;
+            }
+        }
+        return new Data(dia, mes, ano);
+	}
+	
+	public Data sub(int dias){
+		int dia = this.componentes[0];
+		int mes = this.componentes[1];
+		int ano = this.componentes[2];
+        
+        for (int idia = dias; idia > 1; idia--) {
+            
+        	if (dia > 1) dia--;
+            else if (mes > 1) {
+                mes--;
+                dia = diasMes(mes,ano);
+            } else {
+                ano--;
+                mes = 12;
+                dia = 31;
+            }        	
+        }
+        
+        return new Data(dia, mes, ano);
+	}
+	
+	public long sub(Data d){
+		//Subtração 2: Data – Data => dias decorridos entre as duas datas.
+        long diasDiff = 0;
+        int dia = d.componentes[0];
+        int mes = d.componentes[1];
+        int ano = d.componentes[2];
+        
+        int idia = this.componentes[0];
+        int imes = this.componentes[1];
+        int iano = this.componentes[2]; 
+        
+        if (ano < iano || (ano == iano && mes < imes) || 
+           (ano == iano && mes == imes && dia < imes)) {
+            return diasDiff;
+        }
+        
+        while (dia != idia && mes != imes && ano != iano) {
+            if (dia > 1) {
+                dia--;
+                diasDiff++;
+            } else if (mes > 1) {
+                mes--;
+                dia = diasMes(mes,ano);
+                diasDiff++;
+            } else {
+                ano--;
+                mes = 12;
+                dia = 31;
+                diasDiff++;
+            }
+        }
+        
+        return diasDiff;
 	}
 	
 	
